d0a9778185b8266461f28924fa3be47bf03a8db6
Ronald (Jeff) Lopez <lopez.j.ronald@gmail.com>
2020-02-24 14:55:06 -0800

    complete objective2lab2

---
+++ Labs/Objective2Lab2.java
@@ -0,0 +1,9 @@
+public class Objective2Lab2{
+  public static void main(String[] args){
+    String name = "Ronald Lopez";
+    int age = 34;
+    String state = "California";
+
+    System.out.println("Name: " + name + " Age: " + age + " State: " + state);
+  }
+}
