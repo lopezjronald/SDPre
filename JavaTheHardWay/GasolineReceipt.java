e0a27fb8070e2f1039b432295cc1b31ab2d2e24e
Ronald (Jeff) Lopez <lopez.j.ronald@gmail.com>
2020-02-24 14:40:40 -0800

    finish jthw ex 2

---
+++ JavaTheHardWay/GasolineReceipt.java
@@ -0,0 +1,16 @@
+public class GasolineReceipt{
+  public static void main(String[] args){
+    System.out.println( "+------------------------+");
+    System.out.println( "|                        |");
+    System.out.println( "|      CORNER STORE      |");
+    System.out.println( "|                        |");
+    System.out.println( "|    2020-02-24 02:37PM  |");
+    System.out.println( "|                        |");
+    System.out.println( "| Gallons:       10.870  |");
+    System.out.println( "| Price/gallon:  $ 2.999 |");
+    System.out.println( "|                        |");
+    System.out.println( "|  Fuel Total:  $ 32.59  |");
+    System.out.println( "|                        |");
+    System.out.println( "+------------------------+");
+  }
+}
