66340c5b5bef25b4b99573cf94315e72f1a41a4a
Ronald (Jeff) Lopez <lopez.j.ronald@gmail.com>
2020-02-24 13:05:46 -0800

    Exercise 1: Complete

--- JavaTheHardWay/FirstProg.java
+++ JavaTheHardWay/FirstProg.java
@@ -1,5 +1,6 @@
-public class FirstProg{
+public class FirstPro{
   public static void main(String[] args){
-    System.out.println("This is my first program.");
+    System.out.println("I am determined to learn how to code.");
+    System.out.println("Today's date is Feb 24, 2020.");
   }
 }
