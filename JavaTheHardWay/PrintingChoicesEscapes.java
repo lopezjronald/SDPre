cd2b6e7c275fcd938cace46925b5755227465cae
Ronald (Jeff) Lopez <lopez.j.ronald@gmail.com>
2020-02-24 16:36:04 -0800

    finish jthw ex 4.2

---
+++ JavaTheHardWay/PrintingChoicesEscapes.java
@@ -0,0 +1,5 @@
+public class PrintingChoicesEscapes{
+  public static void main(String[] args){
+    System.out.println( "Alpha\nBravo\nCharlie\nDelta\n\nEcho" + "Foxtrot" + "Golf\nHotel\nIndia\n\nThis" + " " + "is" + " " + "a" + " " + "test.\nI" + " " + "am" + " " + "learning" + " " + "Java" + " " + "the" + " " + "hard" + "way" + ".");
+  }
+}
