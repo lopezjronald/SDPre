5b8c8a43976ff1905db3b2cc3734347457564a39
Ronald (Jeff) Lopez <lopez.j.ronald@gmail.com>
2020-02-25 10:54:45 -0800

    finish jthw ex 15

---
+++ JavaTheHardWay/AgeMessages.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+
+public class AgeMessages{
+  public static void main(String[] args){
+    Scanner keyboard = new Scanner(System.in);
+    int age;
+
+    System.out.print("How old are you? ");
+    age = keyboard.nextInt();
+    System.out.println("You are: " + age);
+
+    if (age < 13){
+      System.out.println("\ttoo young to create a Facebook");
+    } if (age < 16) {
+      System.out.println("\ttoo young to get a driver's license");
+    } if (age < 18) {
+      System.out.println("\ttoo young to get a tattoo");
+    } if (age < 21) {
+      System.out.println("\ttoo young to drink alcohol");
+    } if (age < 35) {
+      System.out.println("\ttoo young to run for President of the U.S.");
+      System.out.println("\t\t(How sad!)");
+    } if (age >= 65) {
+      System.out.println("\tYou are old enough to retire!");
+    }
+
+    if (age >= 13){
+      System.out.println("\tOld enough to create a Facebook");
+    } if (age >= 16) {
+      System.out.println("\tOld enough to get a driver's license");
+    } if (age >= 18) {
+      System.out.println("\tOld enough to get a tattoo");
+    } if (age >= 21) {
+      System.out.println("\tOld enough to drink alcohol");
+    } if (age >= 35) {
+      System.out.println("\tOld enough to run for President of the U.S.");
+      System.out.println("\t\t(How awesome!)");
+    } if (age < 65) {
+      System.out.println("\tYou are not old enough to retire!");
+    }
+
+  }
+}
+//Question 1: Nothing gets printed because these are questions dealing with age under instead of over
