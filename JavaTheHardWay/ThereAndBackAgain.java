f59804e752a05cde0e8733b43baa15c5e850f486
Ronald (Jeff) Lopez <lopez.j.ronald@gmail.com>
2020-02-26 14:08:25 -0800

    finish jthw ex 33

---
+++ JavaTheHardWay/ThereAndBackAgain.java
@@ -0,0 +1,15 @@
+public class ThereAndBackAgain {
+  public static void main(String [] args) {
+    System.out.println("Here.");
+    erebor();
+    System.out.println("Back first time.");
+    erebor();
+    System.out.println("Back second time.");
+  }
+
+  //public static void erebor(){
+    //System.out.println("There.");
+  }
+}
+//Question 1: Whe removing the parenthesis, and runtime error occurs
+//Part 2 Question 1: An error occurs when running the program which I predicted. It doesn't recognize the function
