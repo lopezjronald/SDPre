84be1a23d9ad7d82218ba746c6060608d79376cf
Ronald (Jeff) Lopez <lopez.j.ronald@gmail.com>
2020-02-26 15:18:21 -0800

    complete objective6lab4

---
+++ Labs/Objective6Lab4.java
@@ -0,0 +1,10 @@
+public class Objective6Lab4 {
+  public static void main (String [] args){
+    int sum = 0, counter = 1;
+    while (counter <= 20) {
+      sum += counter;
+      counter++;
+    }
+    System.out.println(sum);
+  }
+}
